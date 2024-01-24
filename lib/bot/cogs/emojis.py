9a8418d08ebdb3c222cff691ebf7609bc35aa8c6
Kshitij Tyagi <kshitijtyagiwastaken@gmail.com>
2024-01-24 11:25:48 +0530

    updated emojis

--- lib/bot/cogs/emojis.py
+++ lib/bot/cogs/emojis.py
@@ -10,11 +10,9 @@ class Emojis(commands.Cog):
         self.description = "Don't have Nitro? No problem! Use these emojis instead!"
 
     emoji = app_commands.Group(name='emoji', description='Don\'t have Nitro? No problem! Use these emojis instead!')
-    
     def extract_emojis(self,message):
         emoji_pattern = re.compile(r':([^:\s]+):')
         emojis = re.findall(emoji_pattern, message)
-        # message_without_emojis = re.sub(emoji_pattern, '{}', message)
         return emojis
     
     def new_message(self, message:discord.Message, user_id, emojis):
@@ -37,14 +35,17 @@ class Emojis(commands.Cog):
                 return None
             return emoji_entry.emoji_id
 
-    def get_emoji_ids(self, user_id, emojis):
+    def get_emoji_ids(self, user_id, guild_id, emojis):
         emoji_ids = []
         for emoji in emojis:
             with db.Session() as session:
-                emoji_entry = session.query(db.UserEmoji).filter_by(user_id=user_id, emoji_name=f':{emoji}:').first()
-                if not emoji_entry:
-                    continue
-                emoji_ids.append(emoji_entry.emoji_id)
+                emoji_entry = session.query(db.UserEmoji).join(db.Emoji, db.UserEmoji.emoji_id == db.Emoji.id).filter(
+                    db.UserEmoji.user_id == user_id,
+                    db.UserEmoji.emoji_name == f':{emoji}:',
+                    db.Emoji.guild_id != guild_id
+                ).first()
+                if emoji_entry:
+                    emoji_ids.append(emoji_entry.emoji_id)
         return emoji_ids
 
     @commands.Cog.listener()
@@ -52,28 +53,45 @@ class Emojis(commands.Cog):
         if message.author.bot:
             return
         emojis = self.extract_emojis(message.content)
-        emoji_ids = self.get_emoji_ids(message.author.id, emojis)
+        emoji_ids = self.get_emoji_ids(message.author.id, message.guild.id, emojis)
         if not emoji_ids:
             return
         await message.delete()
-        webhook = await message.channel.create_webhook(name='busty pewpew')
+        webhook = discord.utils.get(await message.channel.webhooks(), name='busty pewpew')
+        if webhook is None:
+            webhook = await message.channel.create_webhook(name='busty pewpew')
         await webhook.send(content=self.new_message(message.content, message.author.id, emojis), username=message.author.display_name, avatar_url=message.author.avatar.url)
-        await webhook.delete()
     
     @emoji.command(name='tutorial', description='Open a tutorial menu to use emojis')
     async def emoji_tutorial(self, interaction:Interaction):
-        pass
+        embed = discord.Embed(
+        title="🤖 Emoji Bot Tutorial",
+        description="Welcome to Emoji Bot! This bot enhances your Discord experience by providing a variety of emojis for your server.",
+        color=0x3498db
+        )
+        await interaction.response.send_message(embed=embed)
+        embed = discord.Embed(color=0x3498db).add_field(name="**1. Adding Emoji**", value="To add an emoji to your personal stash, use the \n`/emoji add :<emoji_name>: <new_name>` command (`<new_name>` is optional).\nExample: `/emoji add :skull:`.\nYou can also add and use animated emojis.\nThe emoji you are adding should be present in the server where this command is executed.\nAfter adding the emoji, you can use it in any server where this bot is present.").set_image(url="https://media.discordapp.net/attachments/1199589417136955432/1199589537496711248/2024-01-24-10-46-17_meFeshg7-ezgif.com-video-to-gif-converter.gif?ex=65c317d1&is=65b0a2d1&hm=f8c16fbffc4f43418b8c70bd625d1431761e81968794daee88d15566517c34ec&=")
+        await interaction.channel.send(embed=embed)
+        embed = discord.Embed(color=0x3498db).add_field(name="**2. Using Emoji**", value="To use an emoji, simply type `:<emoji_name>:` in your message.\nExample: `:happy:`\n").set_image(url="https://cdn.discordapp.com/attachments/1199589417136955432/1199589536662044713/2024-01-24-10-48-48_kqiDJuMV-ezgif.com-video-to-gif-converter.gif?ex=65c317d0&is=65b0a2d0&hm=672738009774f0c64b50ec028d487268771ae88f180186b7524ed1c48f847ca9&")
+        await interaction.channel.send(embed=embed)
+        embed = discord.Embed(color=0x3498db).add_field(name="**3. Listing Emoji**", value="To list all the emojis in your personal stash, use the `/emoji list` command.", inline = False)
+        embed.add_field(name="**4. Removing Emoji**", value="To remove an emoji from your personal stash, use the `/emoji remove <emoji_name>` command.\nExample: `/emoji remove :skull:`" , inline = False)
+        embed.add_field(name="**5. Emoji Id**", value="To get the id of all the emojis in the server, use the `/emoji id` command.", inline = False)
+        await interaction.channel.send(embed=embed)
 
     @emoji.command(name='id', description='Shows id of all the emojis in the server')
-    async def emoji_id_all(self, ctx:commands.Context):
-        embed = discord.Embed(title="Emoji id", description='\n'.join(f"\{emoji}" for emoji in ctx.guild.emojis), color=discord.Color.blurple()).set_thumbnail(url=self.bot.user.avatar)
-        await ctx.send(embed=embed, ephemeral=True)
+    async def emoji_id(self, interaction:Interaction):
+        embed = discord.Embed(title="Emoji id", description='\n'.join(f"\{emoji}" for emoji in interaction.guild.emojis), color=discord.Color.blurple()).set_thumbnail(url=self.bot.user.avatar)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @emoji.command(name='add', description='Add a personal emoji')
     async def emoji_add(self, interaction:Interaction, emoji: str = None, name: str = None):
         if self.has_spaces(emoji) or self.has_spaces(name) or not emoji:
             return await interaction.response.send_message("Invalid emoji name", ephemeral=True)
-        emoji = self.extract_emojis(emoji)[0]
+        try:
+            emoji = self.extract_emojis(emoji)[0]
+        except IndexError:
+            return await interaction.response.send_message("Not found", ephemeral=True)
         if not name:
             name = emoji
         emoji = discord.utils.get(interaction.guild.emojis, name=emoji)
@@ -100,20 +118,22 @@ class Emojis(commands.Cog):
                 emoji_id = f'<a:{emoji.name}:{emoji.id}>'
             else:
                 emoji_id = f'<:{emoji.name}:{emoji.id}>'
-            emoji_entry = db.Emoji(id=emoji_id, guild=guild_entry)
-            session.add(emoji_entry)
+            emoji_entry = session.query(db.Emoji).filter_by(id=emoji_id).first()
+            if not emoji_entry:
+                emoji_entry = db.Emoji(id=emoji_id, guild=guild_entry)
+                session.add(emoji_entry)
             user_emoji_entry = db.UserEmoji(user=user_entry, emoji=emoji_entry, emoji_name=name)
             session.add(user_emoji_entry)
             session.commit()
         await interaction.response.send_message(f"{emoji_id} Emoji added", ephemeral=True)
                 
-
     @emoji.command(name='remove', description='Remove a personal emoji')
-    async def emoji_remove(self, interaction:Interaction, emoji_name:str=None):
+    async def emoji_remove(self, interaction:Interaction, emoji_name:str = None):
         if emoji_name is None or self.has_spaces(emoji_name):
             return await interaction.response.send_message("Invalid emoji name", ephemeral=True)
-        if emoji_name[0] != ':' or emoji_name[-1] != ':':
-            emoji_name = f":{emoji_name}:"
+        if emoji_name[0] == '<' and emoji_name[-1] == '>':
+            emoji_name = emoji_name.split(':')[1]
+        emoji_name = f":{emoji_name}:"
         with db.Session() as session:
             emoji_entry = session.query(db.UserEmoji).filter_by(user_id=interaction.user.id, emoji_name=emoji_name).first()
             if not emoji_entry:
--- lib/db/db.py
+++ lib/db/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, ForeignKey, String, Integer, select
+from sqlalchemy import create_engine, Column, ForeignKey, String, Integer, select, join
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 db = 'sqlite:///lib/db/database.db'
@@ -39,31 +39,10 @@ class UserEmoji(Base):
     __tablename__ = 'user_emoji'
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(String, ForeignKey('user.id'))
-    emoji_id = Column(String, ForeignKey('emoji.id'))
     emoji_name = Column(String, nullable=True)
+    emoji_id = Column(String, ForeignKey('emoji.id'))
 
 def create_tables():
     Base.metadata.create_all(engine)
-    
-# guild = Guild(id = '1195611167805153391')
-# session.add(guild)
-    
-# setting = GuildSetting(guild=guild, prefix='!', welcome_channel_id='1196743047787053097', freebie_channel_id='1197144575597363342', admin_role_id='1196465390381977680')
-# session.add(setting)
-    
-# user = User(id='467620259982213130')
-# session.add(user)
-
-# emoji = Emoji(id = '<a:yourmom:1199035021441388554>', guild=guild)
-# session.add(emoji)
-
-# ue = UserEmoji(user=user, emoji=emoji, emoji_name='yomum')
-# session.add(ue)
-# session.commit()
-    
-# smt = select(UserEmoji).where(UserEmoji.user_id == '467620259982213130')
-# with engine.connect() as conn:
-#     result = conn.execute(smt)
-#     print(result.fetchone()[2])
 
 create_tables()
